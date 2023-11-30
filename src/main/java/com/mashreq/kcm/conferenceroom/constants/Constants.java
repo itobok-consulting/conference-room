653067027dc8077c40228183ee02ce2895981bf3
Koboti Mogale <coffart@gmail.com>
2023-11-30 08:31:47 +0200

    Create Constants.java

---
+++ src/main/java/com/mashreq/kcm/conferenceroom/constants/Constants.java
@@ -0,0 +1,8 @@
+package com.mashreq.kcm.conferenceroom.constants;
+
+public interface Constants {
+    String NO_MEETING_ROOM_AVAILABLE = "There are not rooms available for the specific time range.";
+    String MEETING_ROOM_ALREADY_BOOKED  = "Meeting room already booked.";
+}
+
+
